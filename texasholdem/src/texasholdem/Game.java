7ee664b9b0e3cab256593791dc64abeeaad39e00
Kristian Mendoza <mendozak2@wit.edu>
2025-02-20 15:29:32 -0500

    Game Rounds

--- texasholdem/src/texasholdem/Game.java
+++ texasholdem/src/texasholdem/Game.java
@@ -1,5 +1,18 @@
 package texasholdem;
 
 public class Game {
-
+static int round = 1;
+	
+	Game(){
+	}		
+	
+	public static void currentRound() {
+			System.out.println("Round " + round + "!");
+	}
+	public static void setRound() {
+		round = round + 1;
+	}
+	public static void exitGame() {
+		System.exit(0);
+	}
 }
