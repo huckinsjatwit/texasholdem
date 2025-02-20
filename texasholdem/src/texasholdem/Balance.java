fcbe7fa76f78b24a1575d5635edec4fa07e4c6a9
Jack Huckins <huckinsj@wit.edu>
2025-02-20 15:31:46 -0500

    added other classes

---
+++ texasholdem/src/texasholdem/Balance.java
@@ -0,0 +1,13 @@
+package texasholdem;
+
+public class Balance {
+	int balance=1000;
+	
+	Balance() {
+		
+	}
+	
+	Balance(int setBal) {
+		this.balance=setBal;
+	}
+}
---
+++ texasholdem/src/texasholdem/Pot.java
@@ -0,0 +1,17 @@
+package texasholdem;
+
+public class Pot {
+	static int money=0;
+	
+	
+	Pot() {
+	}
+	
+	public static void addBet(int bet) {
+		money+=bet;
+	}
+	
+	public static void payOut() {
+		
+	}
+}
