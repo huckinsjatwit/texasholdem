75b764cbcf47155f8f971373e571cdffe9f14b97
Kristian Mendoza <mendozak2@wit.edu>
2025-02-26 09:41:28 -0500

    Added player methods

--- texasholdem/src/texasholdem/Player.java
+++ texasholdem/src/texasholdem/Player.java
@@ -1,5 +1,19 @@
 package texasholdem;
 
 public class Player {
+	
+	static boolean fold = false;
+	
+	Player(){
+	}
+	public static void standHand(boolean stand) {
+		fold = true;
+	}
+	public static void foldHand(boolean fold) {
+		
+	}
+	
+	public static void makeBet() {
 
+	}
 }
