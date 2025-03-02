d22f949e521f46b691a83e84774f264767bbf314
Jack Huckins <huckinsj@wit.edu>
2025-03-02 13:42:00 -0500

    Adds methods to check all hand types

--- texasholdem/src/texasholdem/Deck.java
+++ texasholdem/src/texasholdem/Deck.java
@@ -5,8 +5,8 @@ import java.util.Collections;
 import java.util.Random;
 
 public class Deck {
-	static String[] suits={"Hearts", "Clubs", "Spades", "Diamonds"};
-	static int[] values={2,3,4,5,6,7,8,9,10,11,12,13,14};
+	public static String[] suits={"Hearts", "Clubs", "Spades", "Diamonds"};
+	public static int[] values={2,3,4,5,6,7,8,9,10,11,12,13,14};
 	public static Card[] deck= new Card[52];
 	private static final Card dealedCard = new Card("",0);
 
