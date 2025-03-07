2513ddce140e5c448f40fb9940ba7b95278e796a
Jayvon Bernard <bernardj6@wit.edu>
2025-03-07 16:57:21 -0500

    Separated allBetsSame into different methods. highestBet can be implemented for different methods now.

--- texasholdem/src/texasholdem/Pot.java
+++ texasholdem/src/texasholdem/Pot.java
@@ -25,39 +25,62 @@ public class Pot {
 		
 		return pay;
 	}
-	public static boolean allBetsSame(ArrayList numberOfBots) {
+	public static boolean allBetsSame(ArrayList currentPlayers) {
 		//create an array that refreshes every round with size of players
-		int index=0;
-		int highest = 0;
-		boolean isSame = true;
-		if(numberOfBots.size()<bets.size()){
-			index = bets.size()-(numberOfBots.size()); 
-		}else {
-			index = 0;
-			}
-		int [] currentBets = new int [numberOfBots.size()];		//uses the bets array to grab the least x amount of bets where x is the number of players still in the round.
-		for(int i = 0; i<currentBets.length;i++) {
-			currentBets[i] = bets.get(index);		
-			index++;
-		}
-	/* 
-		System.out.println("CurrentBets: ");
-		for(int j = 0; j<currentBets.length; j++) {
-		System.out.print(currentBets[j]+" ");
-		}
-	*/
-		for(int k =0; k < currentBets.length; k++) {
-			if(highest<currentBets[k]) {
-				highest = currentBets[k];
-			}
-		}
+		boolean isSame=true;
+		
 		//System.out.println("Highest: " + highest);
+		int [] currentBets = currentBets(currentPlayers);				
+		int highest = highestBet(currentBets);
+		
 		for(int l = 0; l<currentBets.length;l++) {
 			if(currentBets[l]!=highest) {
 				isSame = false;
 			}
 		}
+		
 		return isSame;
+	}
+	
+	/*
+	 * creates an array of the current round bets using the arraylist of currentPlayers(botCopy) to get the number of current players. 
+	 */
+	public static int[] currentBets(ArrayList currentPlayers) {
+		int index=0;
+		
+		if(currentPlayers.size()<bets.size()){ 			//uses the bets array to grab the least x amount of bets where x is the number of players still in the round.
+			index = bets.size()-(currentPlayers.size()); 		//if the number of current players is greater than or equal to the number of bets in the round, the index would be zero. 
+		}else {													// As long as the botCopy arraylist updates immediately whenever a player or bot folds, it should work fine
+			index = 0;
+			}
+		int [] currentBets = new int [currentPlayers.size()];  
+		for(int i = 0; i<currentBets.length;i++) {
+			currentBets[i] = bets.get(index);		
+			index++;
+		}
+		return currentBets;
+	}
+
+	/*
+	 * pairs with the currentBets method to return the highest bet of the round. Used for checking that all the bets match the highest. Useful for a player call method to make sure the player can't call lower than the highest bet
+	 */
+	public static int highestBet(int[] currentBets){
+		int highest = 0;	
+
+		for(int i = 0; i < currentBets.length; i++) {
+			if(highest<currentBets[i]) {
+				highest = currentBets[i];
+			}
+
+		}
+		/* 
+		System.out.println("CurrentBets: ");					// for testing
+		for(int j = 0; j<currentBets.length; j++) {
+		System.out.print(currentBets[j]+" ");
+		}
+	*/
+		//System.out.println("Highest: " + highest);
+		return highest;
 	}
 	public String toString() {
 		String s="The current pot is: " + currentPot;
